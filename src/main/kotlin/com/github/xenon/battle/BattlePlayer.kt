e7f98476f2fb125afabe5f930fe185470f924adb
devil0414 <yonghyeonjeong7@gmail.com>
2021-04-15 14:15:40 +0900

    Updated System

--- src/main/kotlin/com/github/xenon/battle/BattlePlayer.kt
+++ src/main/kotlin/com/github/xenon/battle/BattlePlayer.kt
@@ -5,7 +5,11 @@ import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.OfflinePlayer
+import org.bukkit.entity.ArmorStand
+import org.bukkit.entity.Entity
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.util.Vector
 import java.util.*
 
@@ -30,18 +34,13 @@ class BattlePlayer(val uniqueId: UUID, name: String) {
         val player = player ?: return
         if(rank == -2) {
             list.forEachIndexed { index, p ->
-                p.teleport(getPlayerLocation(p))
+                p.allowFlight = true
+                p.isFlying = true
+                Bukkit.dispatchCommand(player, "execute as ${player.name} at @s run tp ${p.name} ~ ~${4 * index} ~")
                 if(player.gameMode == GameMode.SPECTATOR) {
                     list.remove(p)
                 }
             }
         }
-    }
-    private fun getPlayerLocation(p: Player): Location {
-        val loc = player!!.location
-        loc.yaw = p.location.yaw
-        loc.pitch = p.location.pitch
-        loc.y += p.location.y
-        return loc
     }
 }
